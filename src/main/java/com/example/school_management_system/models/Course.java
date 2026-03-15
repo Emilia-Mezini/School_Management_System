49cdef9bbac2c78ca378c9a4e22c3b719a016b66
Emilia-Mezini <emiliamezini8@gmail.com>
2026-03-15 20:41:15 +0100

    Completed Phase1: Core Student and Course models.

---
+++ src/main/java/com/example/school_management_system/models/Course.java
@@ -0,0 +1,64 @@
+package com.example.school_management_system.models;
+import java.util.ArrayList;
+import java.util.List;
+public class Course {
+    private final String course_id;
+    private String title;
+    private int numberOfCredits;
+    private List<Student> enrolledStudents;
+
+    public Course(String course_id,String title,int numberOfCredits){
+        this.course_id = course_id;
+        this.title = title;
+        this.numberOfCredits = numberOfCredits;
+        enrolledStudents = new ArrayList<>();
+    }
+
+    public String getCourseId(){
+        return this.course_id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getNumberOfCredits(){
+        return this.numberOfCredits;
+    }
+
+    public List<Student> getEnrolledStudents(){ return this.enrolledStudents; }
+
+    /* Courses name can be updated in the future*/
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    /* To meet new standards the credits number for a course can change */
+    public void setNumberOfCredits(int numberOfCredits){
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    /* A course should have registered students */
+    public void register(Student student){
+        if(!enrolledStudents.contains(student)) {
+            enrolledStudents.add(student);
+            student.enroll(this);
+        }
+    }
+
+    public void deregister(Student student){
+        if(enrolledStudents.contains(student)){
+            enrolledStudents.remove(student);
+            student.unenroll(this);
+        }
+    }
+
+    /* We would like to print out the names of the course participants */
+    public String printParticipantsNames(){
+        StringBuilder sb = new StringBuilder();
+        for( Student student: enrolledStudents ){
+            sb.append(student.getName()).append(",");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/example/school_management_system/models/Student.java
@@ -0,0 +1,73 @@
+package com.example.school_management_system.models;
+import java.util.ArrayList;
+import java.util.List;
+public class Student {
+    private String name;
+    private String email;
+    private final int student_id;
+    private List<Course> coursesEnrolled;
+
+
+    public Student(String name, String email,int student_id){
+        this.name = name;
+        this.email = email;
+        this.student_id = student_id;
+        this.coursesEnrolled = new ArrayList<>();
+    }
+
+    public int getStudentId(){
+        return this.student_id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public List<Course> getCoursesEnrolled(){
+        return this.coursesEnrolled;
+    }
+
+    /* A Student might change his/her email */
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    /* A student should be able to enroll in a course */
+    public void enroll(Course course) {
+        if (!this.isEnrolled(course)) {
+            coursesEnrolled.add(course);
+            course.register(this);
+        }
+    }
+
+    /* Check if a Student is already enrolled in a course */
+    public boolean isEnrolled(Course course){
+        return coursesEnrolled.contains(course);
+    }
+
+    /* A student should be able to unroll from a course */
+    public void unenroll(Course course){
+        if(this.isEnrolled(course)){
+            coursesEnrolled.remove(course);
+            course.deregister(this);
+        }
+    }
+
+    public int getTotalCredits(){
+        int totalCredits = 0;
+        for(Course course : coursesEnrolled){
+            totalCredits += course.getNumberOfCredits();
+        }
+        return totalCredits;
+    }
+
+    @Override
+    public String toString(){
+        return name + " (ID:" + student_id + ")";
+    }
+
+}
